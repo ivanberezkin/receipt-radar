9bef362cdc6721f5c5b72b0d1d160377b9cc9d39
ivbe0111 <berezkinivan94@gmail.com>
2026-04-01 12:32:07 +0200

    added test Successfuly add to receiptlist

--- frontend/src/api/apiService.test.ts
+++ frontend/src/api/apiService.test.ts
@@ -5,10 +5,11 @@ import {
 } from './apiService';
 import { server } from '../mocks/server';
 import { http, HttpResponse } from 'msw';
+import { Receipt } from '../types/receipt';
 
 const API_URL = 'http://localhost:8080/api/receipts';
 
-describe('apiService Error handling', () => {
+describe('apiService Tests', () => {
   describe('addNewReceipts tests', () => {
     it('throw error when server returns status 500', async () => {
       server.use(
@@ -25,6 +26,26 @@ describe('apiService Error handling', () => {
           category: 'test',
         })
       ).rejects.toThrow();
+    });
+
+    it('Successfully add new receipt', async () => {
+      const newReceipt = {
+        vendor: 'Test',
+        amountPaid: 500,
+        date: '2023-02-02',
+        category: 'test',
+      };
+
+      server.use(
+        http.post(API_URL, async ({ request }) => {
+          const body = (await request.json()) as Receipt;
+          return HttpResponse.json({ id: 3, ...body }, { status: 201 });
+        })
+      );
+      const result = await addNewReceipt(newReceipt);
+
+      expect(result).toEqual({ id: 3, ...newReceipt });
+      expect(result.id).toBe(3);
     });
   });
 
