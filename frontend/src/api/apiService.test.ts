1c39af34a8fcbf8e10594777feda1456697053b0
ivbe0111 <berezkinivan94@gmail.com>
2026-04-01 12:17:31 +0200

    added test for error handling for fetch + remove receipts

--- frontend/src/api/apiService.test.ts
+++ frontend/src/api/apiService.test.ts
@@ -1,12 +1,18 @@
-import { addNewReceipt } from './apiService';
+import {
+  addNewReceipt,
+  fetchReceipts,
+  removeReceiptFromApi,
+} from './apiService';
 import { server } from '../mocks/server';
 import { http, HttpResponse } from 'msw';
+
+const API_URL = 'http://localhost:8080/api/receipts';
 
 describe('apiService Error handling', () => {
   describe('addNewReceipts tests', () => {
     it('throw error when server returns status 500', async () => {
       server.use(
-        http.post('*/api/receipts', () => {
+        http.post(API_URL, () => {
           return new HttpResponse(null, { status: 500 });
         })
       );
@@ -19,6 +25,28 @@ describe('apiService Error handling', () => {
           category: 'test',
         })
       ).rejects.toThrow();
+    });
+  });
+
+  describe('fetchReceipts tests', () => {
+    it('throw error on status 404 (not found)', async () => {
+      server.use(
+        http.get(API_URL, () => {
+          return new HttpResponse(null, { status: 404 });
+        })
+      );
+      await expect(fetchReceipts()).rejects.toThrow();
+    });
+  });
+
+  describe('removeReceiptFromApi test', () => {
+    it('throw error on status 404 (not found)', async () => {
+      server.use(
+        http.delete(`${API_URL}/:id`, () => {
+          return new HttpResponse(null, { status: 404 });
+        })
+      );
+      await expect(removeReceiptFromApi(999)).rejects.toThrow();
     });
   });
 });
