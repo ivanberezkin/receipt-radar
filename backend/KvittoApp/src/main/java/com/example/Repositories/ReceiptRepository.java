19c17a3b949c1f2d3d994cd917f244c20398dc9a
ivbe0111 <berezkinivan94@gmail.com>
2026-04-06 14:07:55 +0200

    added findByDateBetween to receiptRepository and implemented it in ReceiptService

--- backend/KvittoApp/src/main/java/com/example/Repositories/ReceiptRepository.java
+++ backend/KvittoApp/src/main/java/com/example/Repositories/ReceiptRepository.java
@@ -4,5 +4,11 @@ package com.example.Repositories;
 import com.example.Model.ReceiptEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
+import java.util.List;
+
 public interface ReceiptRepository extends JpaRepository<ReceiptEntity, Long> {
+
+    List<ReceiptEntity> findByDateBetween(Date start, Date end);
+
 }
--- backend/KvittoApp/src/main/java/com/example/Service/ReceiptServiceImpl.java
+++ backend/KvittoApp/src/main/java/com/example/Service/ReceiptServiceImpl.java
@@ -7,6 +7,9 @@ import com.example.Repositories.ReceiptRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -14,6 +17,42 @@ public class ReceiptServiceImpl implements ReceiptService{
 
     @Autowired
     private ReceiptRepository receiptRepository;
+
+
+
+    @Override
+    public List<ReceiptResponseDto> getAllReceipts() {
+        return receiptRepository.findAll()
+                .stream()
+                .map(this::convertToDto).toList();
+    }
+
+    @Override
+    public List<ReceiptResponseDto> getAllReceiptsForACustomPeriod(String startDate, String endDate) {
+        return receiptRepository.findByDateBetween(formatStringToDate(startDate),formatStringToDate(endDate))
+                .stream()
+                .map(this::convertToDto).toList();
+    }
+
+    @Override
+    public ReceiptResponseDto createReceipt(ReceiptRequestDto newReceipt) {
+        ReceiptEntity savedReceipt = receiptRepository.save(convertToEntity(newReceipt));
+        return convertToDto(savedReceipt);
+    }
+
+    @Override
+    public void deleteReceiptById(Long id) {
+        receiptRepository.deleteById(id);
+    }
+
+    private Date formatStringToDate(String dateToFormat){
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            return formatter.parse(dateToFormat);
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     private ReceiptResponseDto convertToDto(ReceiptEntity receipt){
         ReceiptResponseDto dto = new ReceiptResponseDto();
@@ -37,26 +76,5 @@ public class ReceiptServiceImpl implements ReceiptService{
         return receipt;
     }
 
-    @Override
-    public List<ReceiptResponseDto> getAllReceipts() {
-        return receiptRepository.findAll()
-                .stream()
-                .map(this::convertToDto).toList();
-    }
 
-    @Override
-    public List<ReceiptResponseDto> getAllReceiptsForACustomPeriod(String startDate, String endDate) {
-        return List.of();
-    }
-
-    @Override
-    public ReceiptResponseDto createReceipt(ReceiptRequestDto newReceipt) {
-        ReceiptEntity savedReceipt = receiptRepository.save(convertToEntity(newReceipt));
-        return convertToDto(savedReceipt);
-    }
-
-    @Override
-    public void deleteReceiptById(Long id) {
-        receiptRepository.deleteById(id);
-    }
 }
